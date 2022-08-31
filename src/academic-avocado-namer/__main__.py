b12be4b34826a010ebfa3a6dd2f015b634717a7a
Daren Race <daren.race@gmail.com>
2022-08-31 13:10:30 -0700

    make module main work

--- src/academic-avocado-namer/__main__.py
+++ src/academic-avocado-namer/__main__.py
@@ -1,6 +1,14 @@
 from typing import List, Optional
 from namer import name
+import sys
 
 __version__ = "0.0.1"
 
-print(name())
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        count = int(sys.argv[1])
+    else:
+        count = 1
+
+    for x in range(count):
+        print(name())
--- src/academic-avocado-namer/namer.py
+++ src/academic-avocado-namer/namer.py
@@ -1,9 +1,14 @@
 from word_lists import adjectives, animals
 
 import random
+from datetime import datetime
 
-random.seed(42)
+random.seed(datetime.now())
 
 
 def name(format="<adjective>-<animal>"):
-    return f"{random.choice(adjectives)}-{random.choice(animals)}"
+    adjective = random.choice(adjectives)
+    animal = random.choice(
+        [animal for animal in animals if animal[0] == adjective[0]] or animals
+    )
+    return f"{adjective}-{animal}"
