cc90fc3ba10c59c08ff7902e2b73de97c6ae3863
darenr <daren.race@gmail.com>
2022-09-02 16:34:52 -0700

    update to ver 1.0

--- academic_avocado_namer/__meta__.py
+++ academic_avocado_namer/__meta__.py
@@ -1,7 +1,6 @@
-__version__ = "0.0.5"
+__version__ = "1.0.0"
 __authors__ = "Daren Race"
 __license__ = "Apache License, Version 2.0, January 2004"
-__version__ = "0.1.0"
 __contact__ = "daren.race@gmail.com"
 __license__ = "Apache License"
 __description__ = "Generate names for things with phonetic alliteration - use with GUIDs for easy identification."
