173ae58d47258ac09dce395f021a660d249352a1
darenr <daren.race@gmail.com>
2023-01-07 14:52:18 -0800

    update docs conf extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,9 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.sphinxawesome-theme',
 ]
 
 intersphinx_mapping = {
